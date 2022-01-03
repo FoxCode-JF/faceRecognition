551b54465ce882475af3d924ead4aec6456df688
Fox <filipiukjarek@gmail.com>
2022-01-03 14:49:49 +0100

    bug: not recognizing freshly added face - fixed

--- faceAuthentification.py
+++ faceAuthentification.py
@@ -12,7 +12,6 @@ typeNamePrompt = 'Type person\'s name and press \'1\' to accept'
 
 pictures_path = 'pics'
 attendance_path = 'attendanceLists'
-nameList = []
 images = []
 databaseNames = []
 resizeFactor = 4
@@ -21,7 +20,6 @@ fileNamesList = os.listdir(pictures_path)
 
 detectedFramesCount = 0
 detectedFramesThreshold = 5
-# print(fileNamesList)
 
 for fileName in fileNamesList:
     currentImg = cv2.imread(f'{pictures_path}/{fileName}')
@@ -36,7 +34,14 @@ def findEncodings(images):
         encodedList.append(encodedImg)
     return encodedList
 
-def markPresent(name, nameList):
+def getImageEncoding(imgName):
+    img = cv2.imread(f'{pictures_path}/{imgName}.jpg')
+    images.append(img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    encodedImg = face_recognition.face_encodings(img)[0]
+    return encodedImg
+
+def markPresent(name):
     now = datetime.now()
     str_timestamp = date_time = now.strftime("%d_%m_%Y__%H")
     with open(f'./{attendance_path}/Attendance_{str_timestamp}.csv', 'a+') as f:
@@ -45,22 +50,19 @@ def markPresent(name, nameList):
 
         f.seek(0)
         myDataList = f.readlines()
-        # print(myDataList)
-        
+
+        nameList = []
         for line in myDataList:
             entry = line.split(',')
             nameList.append(entry[0])
         
-        # print(nameList)
+        print(nameList)
         if name not in nameList:
             now = datetime.now()
             time_str = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{time_str}')
 
-
-
 encodedListOfFaces = findEncodings(images)
-print(len(encodedListOfFaces))
 
 videoCapture = cv2.VideoCapture(0)
 
@@ -84,9 +86,9 @@ while True:
             cv2.rectangle(img, (left * resizeFactor, top * resizeFactor), (right * resizeFactor, bottom * resizeFactor), (0, 255 ,0), 3)
             matchName = databaseNames[faceMatchIndex].upper()
             cv2.putText(img, matchName, (left * resizeFactor, top * resizeFactor - 5), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255 ,0))
-            # print(matchName)
-            if detectedFramesCount >= detectedFramesThreshold:
-                markPresent(matchName, nameList=nameList)
+            
+            if matchName not in databaseNames and detectedFramesCount >= detectedFramesThreshold:
+                markPresent(matchName)
                 detectedFramesCount = 0
         else:
             key = cv2.waitKey(1) & 0xFF
@@ -94,22 +96,26 @@ while True:
             inputChar = ''
             if key == ord('y'):
                 success, cleanImg = videoCapture.read()
-                # success, img = videoCapture.read()
                 while (inputChar != '1'):
                     inputChar = chr(cv2.waitKey(1) & 0xFF)
-                    if re.match("[a-z]", inputChar):
+                    if  re.match("[a-z]", inputChar) or \
+                        re.match("[A-Z]", inputChar) or \
+                        re.match("[ ]", inputChar):
                         print(inputChar)
                         myTuple = (newName, inputChar)
                         newName = ''.join(myTuple)
-                    # print("width : {}, height : {}".format(imgMidHorizontal, imgMidVertical))
+                    
                     cv2.putText(img, typeNamePrompt, (15, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0 , 255))
-                    cv2.putText(img, "NEW NAME: {}".format(newName), (imgMidHorizontal, imgMidVertical), cv2.FONT_HERSHEY_COMPLEX, 1.5, (255, 0 , 0))
+                    cv2.putText(img, "NEW NAME:", (imgMidHorizontal - int(imgMidHorizontal / 4), imgMidVertical - 20), cv2.FONT_HERSHEY_COMPLEX, 1.5, (0, 255 , 0))
+                    cv2.putText(img, "{}".format(newName), (imgMidHorizontal - int(imgMidHorizontal / 4), imgMidVertical + 20), cv2.FONT_HERSHEY_COMPLEX, 1.5, (0, 255 , 0))
                     cv2.imshow('WEBCAM', img)
-                # print(newName)
-                cv2.imwrite('{}/{}.jpg'.format(pictures_path, newName), cleanImg)
-                nameList.append(newName)
 
-                # putText(img, unknownPrompt, (left * resizeFactor, top * resizeFactor - 5), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255 ,0))
+                cv2.imwrite('{}/{}.jpg'.format(pictures_path, newName), cleanImg)
+                if newName not in databaseNames:
+                    databaseNames.append(newName)
+                    encodedListOfFaces.append(getImageEncoding(newName))
+                    print(f'LIST OF NAMES\n{databaseNames}\n')
+
             cv2.putText(img, unknownPrompt, (15, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0 , 255))
             cv2.rectangle(img, (left * resizeFactor, top * resizeFactor), (right * resizeFactor, bottom * resizeFactor), (255, 0 ,0), 3)
 
